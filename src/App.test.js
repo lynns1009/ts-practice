8ed33b08065bd0df7671e2d09ebc2e80a7082e79
Liying Shi <lynns1009@gmail.com>
2021-10-10 16:38:44 +1000

    config git igonre file

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,10 @@
-import { render, screen } from '@testing-library/react';
+import { render} from '@testing-library/react';
 import App from './App';
 
 describe('should contain Learn React',()=>{
-  const {getByText,queryByRole} = render(<App />);
+  const {getByText} = render(<App />);
   it('should render Learn React text correctly',()=>{
 
     expect(getByText("Learn React")).toBeInTheDocument();
   });
-});
+})
