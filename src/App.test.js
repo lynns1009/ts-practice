c57bd421c40fd6103b2f1ee7eb3c1959e18e7f33
Liying Shi <lynns1009@gmail.com>
2021-10-10 14:53:01 +1000

    Add jest for test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('should contain Learn React',()=>{
+  it('should render Learn React text correctly',()=>{
+    const {getByText} = render(<App />);
+    expect(getByText("Learn React")).toBeInTheDocument();
+  });
 });
