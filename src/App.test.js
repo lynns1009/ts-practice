d6924ccac77fde1e8ca506641afd3d536e637104
Liying Shi <lynns1009@gmail.com>
2021-10-10 16:15:48 +1000

    test for pre-commit hooks

--- src/App.test.js
+++ src/App.test.js
@@ -6,4 +6,9 @@ describe('should contain Learn React',()=>{
     const {getByText} = render(<App />);
     expect(getByText("Learn React")).toBeInTheDocument();
   });
+
+  it('should render and save to reload text correctly',()=>{
+    const {getByText} = render(<App />);
+    expect(getByText("and save to reload")).toBeInTheDocument();
+  });
 });
