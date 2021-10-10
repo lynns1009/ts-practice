ec40c0ab7921b4a9db6069416728320b2f1d8a85
Liying Shi <lynns1009@gmail.com>
2021-10-10 16:26:54 +1000

    fix typescript dependencies

--- src/App.test.js
+++ src/App.test.js
@@ -2,13 +2,9 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 describe('should contain Learn React',()=>{
+  const {getByText,queryByRole} = render(<App />);
   it('should render Learn React text correctly',()=>{
-    const {getByText} = render(<App />);
-    expect(getByText("Learn React")).toBeInTheDocument();
-  });
 
-  it('should render and save to reload text correctly',()=>{
-    const {getByText} = render(<App />);
-    expect(getByText("and save to reload")).toBeInTheDocument();
+    expect(getByText("Learn React")).toBeInTheDocument();
   });
 });
