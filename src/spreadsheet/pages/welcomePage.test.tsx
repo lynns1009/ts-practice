b7afb4e09603fc772d51c91ad915714ff7ce47a9
Liying Shi <lynns1009@gmail.com>
2021-10-15 16:15:58 +1000

    test

---
+++ src/spreadsheet/pages/welcomePage.test.tsx
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react";
+import React from "react";
+import WelcomePage from "./welcomePage";
+
+describe('the first test practice',()=>{
+    it('should render text correct',()=>{
+        const {getByText} = render(<WelcomePage />)
+        expect(getByText('This is a test')).toBeInTheDocument();
+    })
+});
---
+++ src/spreadsheet/pages/welcomePage.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+const WelcomePage: React.FC = () =>{
+    return <>
+        <div>This is a test</div>
+    </>;
+};
+
+export default WelcomePage
