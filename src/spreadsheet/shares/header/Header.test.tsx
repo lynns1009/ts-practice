c6d736f8895cb687b074328c394b3a5ce2cf6c0b
Liying Shi <lynns1009@gmail.com>
2021-10-25 21:00:38 +1000

    integrate test

--- src/spreadsheet/shares/header/Header.test.tsx
+++ src/spreadsheet/shares/header/Header.test.tsx
@@ -1,14 +1,28 @@
+import { useMediaQuery } from "@mui/material";
 import { render } from "@testing-library/react";
 import React from "react";
 import Header from "./Header";
 
-describe('header should render mobile correctly',()=>{
-
-    it('shoule show up menu icon',()=>{
-        const {getByRole,getByText,container} = render(<Header />);
-        //expect(getByText('Home')).to();
+describe('header should render mobile correctly in desktop screen', () => {
+    beforeAll(() => {
+        (useMediaQuery as jest.Mock).mockReturnValue(true);
+    })
+    it('shoule show up menu icon', () => {
+        const { getByRole, getByText, container } = render(<Header />);
         const mobileDeviceElements = container.getElementsByClassName('mobileDevice');
         expect(mobileDeviceElements.length).toBe(1);
         expect(mobileDeviceElements[0]).toBeVisible();
-    })
+    });
+})
+
+describe('header should render mobile correctly in mobile device', () => {
+
+    it('shoule show up menu icon', () => {
+        const { getByRole, getByText, container } = render(<Header />);
+        const mobileDeviceElements = container.getElementsByClassName('mobileDevice');
+        expect(mobileDeviceElements.length).toBe(1);
+        expect(mobileDeviceElements[0]).toBeVisible();
+    });
+
+
 })
--- src/spreadsheet/shares/header/mobileDeviceMenu/MobileMenu.test.tsx
+++ src/spreadsheet/shares/header/mobileDeviceMenu/MobileMenu.test.tsx
@@ -1,18 +1,38 @@
-import { render } from "@testing-library/react";
 import React, { useState as useStateMock } from "react";
 import MobileMenu from "./MobileMenu";
-import { shallow } from 'enzyme';
+import { shallow, ShallowWrapper } from 'enzyme';
 import MenuIcon from '@mui/icons-material/Menu';
 import CloseIcon from '@mui/icons-material/Close';
 
 describe('should render mobile menu correctly', () => {
-    const wrapper = shallow(<MobileMenu />);
+    let wrapper: ShallowWrapper;
+    beforeEach(() => {
+        wrapper = shallow(<MobileMenu />);
+    });
 
-    it('shoule find menu icon', () => {
-        expect(wrapper.find(MenuIcon)).toBeTruthy();
-    })
+    it('should switch to close icon if user click on menu icon', () => {
+        const menuIcon = wrapper.find(MenuIcon);
 
-    it('shoule find close icon', () => {
-        expect(wrapper.find(CloseIcon)).toBeTruthy();
-    })
-})
+        menuIcon.simulate('click');
+
+        expect(wrapper.find(CloseIcon)).toHaveLength(1);
+        expect(wrapper.find(MenuIcon)).toHaveLength(0);
+    });
+
+    it('should have menu Icon visible', () => {
+        const menuIcon = wrapper.find(MenuIcon);
+
+        expect(menuIcon).toHaveLength(1);
+    });
+
+    it('should have close Icon visible', () => {
+        const setState = jest.fn();
+        jest
+            .spyOn(React, 'useState')
+            .mockImplementationOnce(() => [false, setState]);
+
+        const closeIcon = wrapper.find(CloseIcon);
+
+        expect(closeIcon).toHaveLength(1);
+    });
+});
